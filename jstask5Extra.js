8fb4d90ff83b42f8cca0207f23ed6a088f2ccf79
thanhnt <nguyentruongthanh0612@gmail.com>
2022-07-11 13:54:25 +0700

    complete task

--- jstask5Extra.js
+++ jstask5Extra.js
@@ -20,9 +20,9 @@ function vat() {
   var vatExport = calcVat(vatPerSalary);
   var totalVat = (vatPerSalary * vatExport) / 100;
   console.log("Họ tên:", host);
-  console.log("Thu nhập chịu thuế",vatPerSalary,"triệu đồng");
-  console.log("Thuế xuất là:",vatExport,"%");
-  console.log("Thuế thu nhập cá nhân là:", totalVat,"triệu đồng");
+  console.log("Thu nhập chịu thuế", vatPerSalary, "triệu đồng");
+  console.log("Thuế xuất là:", vatExport, "%");
+  console.log("Thuế thu nhập cá nhân là:", totalVat, "triệu đồng");
 }
 vat();
 
@@ -52,17 +52,52 @@ function calcVat(vatPerSalary) {
 }
 
 
-var customer1 = "Nhà Dân";
-var customer2 = "Doanh Nghiệp";
-var bill = 4.5;
-var basicService = 20.5;
-var channel = 10;
-var totalsub
 
-
-function calcChannel(){
-
+/*Bài 2
+ * Input: Loại khách hàng gồm nhà dân và doanh nghiệp, mã khách hàng, số kênh cao cấp,
+ *        số kênh cao cấp, số kết nối riêng cho doanh nghiệp
+ * Process:
+ *       1. Lấy được input.
+ *       2. Thiết lập tính toán tiền cáp theo loại của khách hàng
+ *       3. Xuất loại khách hàng, mã khách hàng, tiền cáp ra màn hình 
+ *Output: Loại khách hàng, mã khách hàng, tiền cáp.
+ */
+var customer = "Nhà dân";
+// var customer = "Doanh nghiệp";
+var codePersonal = 38505;
+var codeBusiness = 46905;
+var numberOfChannels = 11;
+var connection = 11;
+var Personal;
+var totalPersonal = calcPersonal(Personal);
+var Business;
+var totalBusiness = calcBusiness(Business);
+if (customer === "Nhà dân") {
+  console.log(
+    "Mã khách hàng cá nhân:",
+    codePersonal,
+    "; Tiền cáp:",
+    totalPersonal,
+    "$"
+  );
+} else if (customer === "Doanh nghiệp") {
+  console.log(
+    "Mã khách hàng doanh nghiệp:",
+    codeBusiness,
+    "; Tiền cáp:",
+    totalBusiness,
+    "$"
+  );
 }
-
-
-
+function calcPersonal() {
+  var Personal = 4.5 + 20.5 + numberOfChannels * 7.5;
+  return Personal;
+}
+function calcBusiness() {
+  if (connection <= 10 && connection > 0) {
+    var Business = 75 + 15 + numberOfChannels * 50;
+  } else {
+    var Business = 75 + (connection - 10) * 5 + 15 + numberOfChannels * 50;
+  }
+  return Business;
+}
